2f08c5607f2690e3f52edaea9ee6bffa8e03abe9
Adesh <Adeshpokhrel15@gmail.com>
2022-02-17 23:21:25 +0545

    login

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,6 +1,8 @@
 
 // secret variable for JWT token strategy and give secret key. Can use anything here
 
+// In place of password try to hit your mongoDB password
+
 module.exports = {
     secret: 'yoursecret',
     database: 'mongodb+srv://adeshflutter:<Password>@cluster0.fkxal.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
